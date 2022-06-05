4ca4acafb6121dedb7b32bf125e6eb3d90c008f3
Shivang Soni <shivangsoni1998@gmail.com>
2022-06-04 23:34:22 -0400

    front-end completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { Header } from './Components/header/Header';
+import Instruction from './Components/instructions/Instruction';
+import Form from './Components/form/Form';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Header />
+     <Instruction />
+     <Form />
     </div>
   );
 }
